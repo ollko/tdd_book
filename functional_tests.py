ad0b298e299003d96c777a587329d7c92b25e954
Olga <korotkaya.olga@yandex.ru>
2023-08-02 14:09:10 +0300

    First functional test is extended

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8008")
+class NewVisitorTest(unittest.TestCase):
+    '''New visitor test'''
 
-assert "The install worked successfully! Congratulations!" in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        self.browser.get("http://localhost:8008")
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("End test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
