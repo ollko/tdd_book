827a923b77866168644eb98454067d4928ba0d21
Olga <korotkaya.olga@yandex.ru>
2023-08-03 18:40:37 +0300

    FT check element enter functionality now

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +16,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get("http://localhost:8008")
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
         self.fail("End test!")
 
 
